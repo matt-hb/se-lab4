7af1f979839153fe854b21e41ad34bafcf4d5d87
unknown <185186@BME.HU>
2024-11-06 17:43:17 +0100

    added unit tests for TorpedoStore

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,38 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+        assertEquals(true, store.isEmpty());
     }
+
+    @Test
+    void fire_Fail() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1, 1.0);
+
+        // Act
+        boolean result = store.fire(1);
+
+        // Assert
+        assertEquals(false, result);
+        assertEquals(1, store.getTorpedoCount());
+    }
+
+    @Test
+    void fire_MoreThanStored() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act, Assert
+        assertThrows(IllegalArgumentException.class, () -> store.fire(2));
+    }
+
+    @Test
+    void fire_Zero () {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act, Assert
+        assertThrows(IllegalArgumentException.class, () -> store.fire(0));
+    }
+
 }
